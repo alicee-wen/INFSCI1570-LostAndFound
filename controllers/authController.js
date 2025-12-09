51ed3cf6d149b9cd0df99cd1c4e1f24a7aebecb9
awen <114027298+alicee-wen@users.noreply.github.com>
2025-12-09 00:13:46 -0500

    added comments to admin stuff

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,7 +31,9 @@ module.exports.signup = async (req, res) => {
         const password_hash = await bcrypt.hash(password, 12);
         const _id = await generateUserId(); // custom ID generator
 
-         const isAdmin = (email === "alice.wen2130@gmail.com");
+         const isAdmin = (email === "alice.wen2130@gmail.com"); 
+         // made myself admin to test site because I don't have access to database, 
+         // obviously wouldn't keep this in the real deployed app
 
         const user = new User({
             _id,
